0a8a53b6542fc5efa835a1213aaba9144d41a228
danamlund <dan@danamlund.dk>
2020-12-15 21:16:33 +0100

    .

--- adventofcode2020/recordscreen.sh
+++ adventofcode2020/recordscreen.sh
@@ -6,7 +6,7 @@ if [ "$1" != "" ]; then
 fi
 
 ffmpeg -video_size 3839x2160 -f x11grab -i :0.0 -c:v libx265 -crf 28 -preset medium \
-       -vf "scale=-2:1080,setpts=0.0667*PTS" $out
+       -vf "scale=-2:1080,setpts=PTS*2/30" $out
 
 # 30 fps to 1 fps: 1/30 = 0.0333
 # 30 fps to 2 fps: 2/30 = 0.0667
@@ -17,4 +17,7 @@ ffmpeg -video_size 3839x2160 -f x11grab -i :0.0 -c:v libx265 -crf 28 -preset med
 # ffmpeg -i input.mp4 -c:v libx265 -crf 28 -preset medium output.mp4
 
 # speed up
-# ffmpeg -i input.mp4 -c:v libx265 -crf 28 -preset medium -vf "setpts=2.0*PTS" output.mp4
+# ffmpeg -i input.mp4 -c:v libx265 -crf 28 -preset medium -vf "setpts=0.2*PTS" output.mp4
+
+# downscale
+# ffmpeg -i input.mp4 -c:v libx265 -crf 28 -preset medium -vf "scale=-2:1080" output.mp4
