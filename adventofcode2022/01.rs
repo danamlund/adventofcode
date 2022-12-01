ea1fd8aaba2ad28af630de510728a88fb46ded4d
danamlund <dan@danamlund.dk>
2022-12-01 21:41:22 +0100

    day1 rust

---
+++ adventofcode2022/01.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+
+fn star1() {
+   let mut sum : i64 = 0;
+   let mut maxsum = sum;
+   for line in io::BufReader::new(File::open("01.txt").expect("")).lines().flat_map(|l| l) {
+      if line == "" {
+        if sum > maxsum { maxsum = sum; }
+        sum = 0;
+      } else {
+        let calories : i64 = line.parse().expect("");
+        sum += calories;
+      }
+   }
+
+   println!("= {}", maxsum);
+}
+
+fn star2() {
+   let mut sum : i64 = 0;
+   let mut top3 : Vec<i64> = Vec::new();
+   for line in io::BufReader::new(File::open("01.txt").expect("")).lines().flat_map(|l| l) {
+      if line == "" {
+        top3.push(sum);
+        if top3.len() > 3 {
+          top3.sort_by(|a,b| b.cmp(a));
+          top3.resize(3, 0);
+        }
+        sum = 0;
+      } else {
+        let calories : i64 = line.parse().expect("");
+        sum += calories;
+      }
+   }
+
+   let sum : i64 = top3.iter().sum();
+   println!("= {}", sum);
+}
+
+fn main() {
+   star1();
+   star2();
+}
